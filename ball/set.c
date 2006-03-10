5c694ae68354e2a65c4326351a364fa2cbb7d6d7
mrtout <mrtout@78b8d119-cf0a-0410-b17c-f493084dd1d7>
2006-03-10 16:46:19 +0000

    Fix segfault bug
    git-svn-id: https://s.snth.net/svn/neverball/trunk@311 78b8d119-cf0a-0410-b17c-f493084dd1d7

--- ball/set.c
+++ ball/set.c
@@ -568,7 +568,7 @@ void set_finish_level(struct level_game *lg, const char *player)
 	/* On fail, identify the next level (only in bonus for challenge) */
 	nl = next_normal_level(ln);
 	/* Next level may be unavailable */
-	if (!cl->is_bonus && nl->is_locked)
+	if (!cl->is_bonus && nl != NULL && nl->is_locked)
 	    nl = NULL;
 	/* Fail a bonus level but win the set! */
 	else if (nl == NULL && lg->mode == MODE_CHALLENGE)
