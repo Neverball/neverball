29a3b7c136afb1d1379c4d123a1ff77363813056
Jānis Rūcis <parasti@gmail.com>
2006-10-12 15:34:28 +0000

    In convert-replays.bat, use a separate variable for the name of the executable.
    git-svn-id: https://s.snth.net/svn/neverball/trunk@592 78b8d119-cf0a-0410-b17c-f493084dd1d7

--- tools/convert-replays.bat
+++ tools/convert-replays.bat
@@ -3,6 +3,7 @@
 setlocal enableextensions
 
     set DCEXEC=democonv
+    set DCCMD=%DCEXEC%
 
     if (%1) == () (
     :usage
@@ -10,8 +11,9 @@ setlocal enableextensions
         %DCEXEC% --help 2>&1 | more +1
         echo   --prefix ^<pref^>
         echo            Add this prefix before filenames of converted
-        echo            replays.  Useful for saving replays to a different
-        echo            directory instead of CWD.
+        echo            replays.  Useful for saving replays to a directory
+        echo            other than CWD.
+
         exit /b 1
     )
 
@@ -22,22 +24,22 @@ setlocal enableextensions
         ) else if (%1) == (--help) (
             goto usage
         ) else if (%1) == (--goal) (
-            set DCEXEC=%DCEXEC% %1
+            set DCCMD=%DCCMD% %1
         ) else if (%1) == (--fall-out) (
-            set DCEXEC=%DCEXEC% %1
+            set DCCMD=%DCCMD% %1
         ) else if (%1) == (--time-out) (
-            set DCEXEC=%DCEXEC% %1
+            set DCCMD=%DCCMD% %1
         ) else if (%1) == (--best-time) (
-            set DCEXEC=%DCEXEC% %1
+            set DCCMD=%DCCMD% %1
         ) else if (%1) == (--most-coins) (
-            set DCEXEC=%DCEXEC% %1
+            set DCCMD=%DCCMD% %1
         ) else if (%1) == (--freestyle) (
-            set DCEXEC=%DCEXEC% %1
+            set DCCMD=%DCCMD% %1
         ) else if (%1) == (--player) (
-            set DCEXEC=%DCEXEC% %1 %2
+            set DCCMD=%DCCMD% %1 %2
             shift
         ) else if (%1) == (--date) (
-            set DCEXEC=%DCEXEC% %1 %2
+            set DCCMD=%DCCMD% %1 %2
             shift
         ) else if (%1) == (--prefix) (
             set PREFIX=%2
@@ -49,8 +51,9 @@ setlocal enableextensions
     for %%d in (%*) do (
         rem This ugly test tries to make sure we're not processing command
         rem line options and directories (weird errors pop up).
+
         if exist %%d if not exist %%dnul (
-            %DCEXEC% < %%d > %PREFIX%%%~nxd.nbr
+            %DCCMD% < %%d > %PREFIX%%%~nxd.nbr
             if errorlevel 1 del %PREFIX%%%~nxd.nbr
         )
     )
