7be014cdb02d150dfecccebefa83db0ba85c87e1
Jānis Rūcis <parasti@gmail.com>
2007-07-29 12:34:55 +0000

    Discovered svnversion...
    git-svn-id: https://s.snth.net/svn/neverball/trunk@1092 78b8d119-cf0a-0410-b17c-f493084dd1d7

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rev=$(LC_ALL=C svn info | grep "^Revision:" | cut -c 11-)
-test -n "$rev" && echo "dev-r${rev}"
+rev=$(svnversion . /svn/neverball/trunk)
+test -n "$rev" && test "$rev" != "exported" && echo "dev-r${rev}"
 
