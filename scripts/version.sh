70473f0ab95aff738dc5dc393ef570420f454ef7
Jānis Rūcis <parasti@gmail.com>
2007-10-04 19:55:56 +0000

    Modified version shell script to output a dummy version string if the working directory is a local SVK branch.  (As opposed to a direct mirror checkout.)
    git-svn-id: https://s.snth.net/svn/neverball/trunk@1188 78b8d119-cf0a-0410-b17c-f493084dd1d7

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,14 +5,18 @@ export LC_ALL
 
 svn_version() {
     svn_rev="$(svnversion . /svn/neverball/trunk | tr : +)"
-    test "$svn_rev" != "exported" && echo "$svn_rev"
+    test "$svn_rev" != "exported" && echo "r$svn_rev"
 }
 
 svk_version() {
-    svk info 2> /dev/null | grep -E "^Mirrored From:" |
-    sed -e "s/^[^,]*, Rev\. //"
+    if ! svk info &> /dev/null; then
+        return
+    fi
+
+    svk_rev="$(svk info | grep -E "^Mirrored From:" | cut -d, -f2 | cut -c7-)"
+    test -n "$svk_rev" && echo "r$svk_rev" || echo "svk"
 }
 
-rev="$(svn_version || svk_version)"
-test -n "$rev" && echo "dev-r$rev" || echo "unknown"
+version="$(svn_version || svk_version)"
+test -n "$version" && echo "dev-$version" || echo "unknown"
 
