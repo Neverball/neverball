2765d143bdf06420100a5b1a531604d2d3acefc8
Jānis Rūcis <parasti@gmail.com>
2007-11-03 13:20:31 +0000

    Simplified version script.
    Only  Subversion  working copies  are  now  supported. For  SVK,  git,
    non-versioned trees, etc, the date is used instead.
    
    
    git-svn-id: https://s.snth.net/svn/neverball/trunk@1208 78b8d119-cf0a-0410-b17c-f493084dd1d7

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,20 +3,12 @@
 LC_ALL=C
 export LC_ALL
 
-svn_version() {
+svn_version()
+{
     svn_rev="$(svnversion . /svn/neverball/trunk | tr : +)"
     test "$svn_rev" != "exported" && echo "r$svn_rev"
 }
 
-svk_version() {
-    if ! svk info &> /dev/null; then
-        return
-    fi
-
-    svk_rev="$(svk info | grep -E "^Mirrored From:" | cut -d, -f2 | cut -c7-)"
-    test -n "$svk_rev" && echo "r$svk_rev" || echo "svk"
-}
-
-version="$(svn_version || svk_version)"
-test -n "$version" && echo "dev-$version" || echo "unknown"
+version="$(svn_version || date --utc +"%Y-%m-%d")"
+test -n "$version" && echo "$version" || echo "unknown"
 
