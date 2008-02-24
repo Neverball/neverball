726ef648eea19aafcef43ddb8c82f4ba8489cacc
Byron Johnson <byron@byronjohnson.net>
2008-02-24 22:07:46 +0000

    Cleaned up balls
    git-svn-id: https://s.snth.net/svn/neverball/branches/putt-collisions@1820 78b8d119-cf0a-0410-b17c-f493084dd1d7

--- share/config.h
+++ share/config.h
@@ -154,7 +154,7 @@ enum {
 #define DEFAULT_ROTATE_SLOW          100
 #define DEFAULT_ROTATE_FAST          200
 #define DEFAULT_PLAYER               ""
-#define DEFAULT_BALL                 "ball/basic-ball/basic-ball"
+#define DEFAULT_BALL                 "basic-ball"
 #define DEFAULT_CHEAT                0
 #define DEFAULT_KEY_FORWARD          SDLK_UP
 #define DEFAULT_KEY_BACKWARD         SDLK_DOWN
