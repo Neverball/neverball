e6288bbb2bb8e6d165440fef3b75b63495843052
Ben Anderson <jammnrose+github@gmail.com>
2008-07-01 03:37:41 +0000

    Organized shell script to remove files in a similar order to their installation.
    git-svn-id: https://s.snth.net/svn/neverball/trunk@2374 78b8d119-cf0a-0410-b17c-f493084dd1d7

--- macosx/Neverball_Uninstall_Script.sh
+++ macosx/Neverball_Uninstall_Script.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo rm -fr /Library/Application\ Support/Neverball\ Data /Library/Receipts/Neverball_Data.pkg /Library/Receipts/Neverball_Docs.pkg /Library/Receipts/Neverball.pkg /Library/Receipts/Neverputt.pkg /Applications/Neverputt.app /Applications/Neverball.app /Library/Receipts/Neverball_Uninstall_Script.pkg
+sudo rm -fr /Library/Receipts/Neverball_Uninstall_Script.pkg /Library/Receipts/Neverball.pkg /Library/Receipts/Neverputt.pkg /Library/Receipts/Neverball_Data.pkg /Library/Receipts/Neverball_Docs.pkg /Library/Application\ Support/Neverball\ Data /Applications/Neverball.app /Applications/Neverputt.app
