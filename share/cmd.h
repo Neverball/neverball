f958793e77e785eb275a85a91cae15b5bec129ab
Jānis Rūcis <parasti@gmail.com>
2010-10-21 17:03:41 +0000

    Update copyright headers
    git-svn-id: https://s.snth.net/svn/neverball/trunk@3318 78b8d119-cf0a-0410-b17c-f493084dd1d7

--- share/cmd.h
+++ share/cmd.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 Neverball contributors
+ * Copyright (C) 2009 Neverball authors
  *
  * NEVERBALL is  free software; you can redistribute  it and/or modify
  * it under the  terms of the GNU General  Public License as published
