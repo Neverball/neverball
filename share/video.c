69354014eba2451616bb7a742dbf94666ebed9e9
Jānis Rūcis <parasti@gmail.com>
2011-07-28 13:59:20 +0000

    Cheat
    git-svn-id: https://s.snth.net/svn/neverball/trunk@3625 78b8d119-cf0a-0410-b17c-f493084dd1d7

--- share/video.c
+++ share/video.c
@@ -309,10 +309,12 @@ void video_pop_matrix(void)
 void video_clear(void)
 {
     if (config_get_d(CONFIG_REFLECTION))
-        glClear(GL_DEPTH_BUFFER_BIT |
+        glClear(GL_COLOR_BUFFER_BIT |
+                GL_DEPTH_BUFFER_BIT |
                 GL_STENCIL_BUFFER_BIT);
     else
-        glClear(GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT |
+                GL_DEPTH_BUFFER_BIT);
 }
 
 /*---------------------------------------------------------------------------*/
