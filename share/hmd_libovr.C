2e7b493c9c69668fddd546078b7eab19f8f785a7
Robert Kooima <robert.kooima@gmail.com>
2013-08-09 15:04:15 +0000

    Added fallback defaults when no Oculus device is connected.
    git-svn-id: https://s.snth.net/svn/neverball/branches/hmd@3939 78b8d119-cf0a-0410-b17c-f493084dd1d7

--- share/hmd_libovr.C
+++ share/hmd_libovr.C
@@ -16,6 +16,7 @@
 
 extern "C"
 {
+    #include "config.h"
     #include "glext.h"
     #include "glsl.h"
     #include "fbo.h"
@@ -30,9 +31,8 @@ static OVR::Ptr<OVR::SensorDevice>  pSensor;
 static OVR::HMDInfo      Info;
 static OVR::SensorFusion Fusion;
 
-static OVR::Util::Render::StereoConfig Stereo;
-
-static int eye = 0;
+static OVR::Util::Render::StereoConfig    Stereo;
+static OVR::Util::Render::StereoEyeParams Params;
 
 /*---------------------------------------------------------------------------*/
 
@@ -82,11 +82,11 @@ static const char *hmd_frag[] = {
     "vec2 HMDWarp(vec2 c)\n",
     "{\n",
         "vec2 theta = (c - LensCenter) * ScaleIn;\n",
-        "float rSq = theta.x * theta.x + theta.y * theta.y;\n",
-        "vec2 rvector = theta * (DistortionK.x + "
-                                "DistortionK.y * rSq + "
-                                "DistortionK.z * rSq * rSq + "
-                                "DistortionK.w * rSq * rSq * rSq);\n",
+        "float rr = theta.x * theta.x + theta.y * theta.y;\n",
+        "vec2 rvector = theta * (DistortionK.x + \n",
+                                "DistortionK.y * rr + \n",
+                                "DistortionK.z * rr * rr + \n",
+                                "DistortionK.w * rr * rr * rr);\n",
         "return LensCenter + ScaleOut * rvector;\n",
     "}\n",
 
@@ -118,22 +118,22 @@ static void hmd_gl_init()
 
     glUseProgram_  (L_glsl.program);
     glsl_uniform2f(&L_glsl, "LensCenter", 0.5 + 0.5f * d, 0.5);
+    glsl_uniform2f(&L_glsl, "ScaleOut", 0.5f * s, 0.5f * s * a);
+    glsl_uniform2f(&L_glsl, "ScaleIn",  2.0f,     2.0f     / a);
     glsl_uniform4f(&L_glsl, "DistortionK", Info.DistortionK[0],
                                            Info.DistortionK[1],
                                            Info.DistortionK[2],
                                            Info.DistortionK[3]);
-    glsl_uniform2f(&L_glsl, "ScaleOut", 0.5f * s, 0.5f * s * a);
-    glsl_uniform2f(&L_glsl, "ScaleIn",  2.0f,     2.0f     / a);
 
 
     glUseProgram_  (R_glsl.program);
+    glsl_uniform2f(&L_glsl, "ScaleOut", 0.5f * s, 0.5f * s * a);
+    glsl_uniform2f(&L_glsl, "ScaleIn",  2.0f,     2.0f     / a);
     glsl_uniform2f(&R_glsl, "LensCenter", 0.5 - 0.5f * d, 0.5);
     glsl_uniform4f(&R_glsl, "DistortionK", Info.DistortionK[0],
                                            Info.DistortionK[1],
                                            Info.DistortionK[2],
                                            Info.DistortionK[3]);
-    glsl_uniform2f(&L_glsl, "ScaleOut", 0.5f * s, 0.5f * s * a);
-    glsl_uniform2f(&L_glsl, "ScaleIn",  2.0f,     2.0f     / a);
 
     glUseProgram_(0);
 
@@ -150,12 +150,15 @@ static void hmd_gl_init()
 
 static void hmd_gl_draw(GLuint program, GLuint texture, GLuint buffer)
 {
-    glUseProgram_(program);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glBindBuffer_(GL_ARRAY_BUFFER, buffer);
-    glVertexPointer  (2, GL_FLOAT, sizeof (struct point), (GLvoid *) 0);
-    glTexCoordPointer(2, GL_FLOAT, sizeof (struct point), (GLvoid *) 8);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    if (buffer)
+    {
+        glUseProgram_(program);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindBuffer_(GL_ARRAY_BUFFER, buffer);
+        glVertexPointer  (2, GL_FLOAT, sizeof (struct point), (GLvoid *) 0);
+        glTexCoordPointer(2, GL_FLOAT, sizeof (struct point), (GLvoid *) 8);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    }
 }
 
 static void hmd_gl_free()
@@ -198,11 +201,37 @@ static void hmd_gl_matrix(OVR::Matrix4f& M)
 
 extern "C" int hmd_stat()
 {
-    return OVR::System::IsInitialized() ? 1 : 0;
+    return 1;
+    // return (OVR::System::IsInitialized() && pHMD) ? 1 : 0;
 }
 
 extern "C" void hmd_init()
 {
+    // Set default HMD info for a 7" OVR DK1 in case OVR fails for any reason.
+
+    Info.DesktopX               = 0;
+    Info.DesktopY               = 0;
+    Info.HResolution            = config_get_d(CONFIG_WIDTH);
+    Info.VResolution            = config_get_d(CONFIG_HEIGHT);
+
+    Info.HScreenSize            =  0.14976f;
+    Info.VScreenSize            =  0.09350f;
+    Info.InterpupillaryDistance =  0.0640f;
+    Info.LensSeparationDistance =  0.0635f;
+    Info.EyeToScreenDistance    =  0.0410f;
+    Info.VScreenCenter          =  Info.VScreenSize * 0.5f;
+
+    Info.DistortionK[0]         =  1.00f;
+    Info.DistortionK[1]         =  0.22f;
+    Info.DistortionK[2]         =  0.24f;
+
+    Info.ChromaAbCorrection[0]  =  0.996f;
+    Info.ChromaAbCorrection[1]  = -0.004f;
+    Info.ChromaAbCorrection[2]  =  1.014f;
+    Info.ChromaAbCorrection[3]  =  0.000f;
+
+    // Initialize OVR, the device, the sensor, and the sensor fusion.
+
     OVR::System::Init(OVR::Log::ConfigureDefaultLog(OVR::LogMask_All));
 
     if (OVR::System::IsInitialized())
@@ -211,104 +240,108 @@ extern "C" void hmd_init()
         {
             if ((pHMD = *pManager->EnumerateDevices<OVR::HMDDevice>().CreateDevice()))
             {
-                pHMD->GetDeviceInfo(&Info);
-
                 if ((pSensor = *pHMD->GetSensor()))
                 {
                     Fusion.AttachToSensor(pSensor);
 
-                    Stereo.SetFullViewport(OVR::Util::Render::Viewport(0, 0, Info.HResolution, Info.VResolution));
-                    Stereo.SetStereoMode(OVR::Util::Render::Stereo_LeftRight_Multipass);
-                    Stereo.SetHMDInfo(Info);
-                    Stereo.SetDistortionFitPointVP(-1.0f, 0.0f);
+                    pHMD->GetDeviceInfo(&Info);
 
-                    hmd_gl_init();
                 }
             }
         }
     }
+
+    // Set up the stereo config using the HMD info.
+
+    using namespace OVR::Util::Render;
+
+    Stereo.SetHMDInfo(Info);
+    Stereo.SetDistortionFitPointVP(-1.0f, 0.0f);
+    Stereo.SetStereoMode(Stereo_LeftRight_Multipass);
+    Stereo.SetFullViewport(Viewport(0, 0, Info.HResolution,
+                                          Info.VResolution));
+    hmd_gl_init();
 }
 
 extern "C" void hmd_free()
 {
     hmd_gl_free();
+
+    pSensor  = 0;
+    pHMD     = 0;
+    pManager = 0;
+
     // OVR::System::Destroy();
 }
 
 extern "C" void hmd_step()
 {
+    /* LibOVR is threaded. No synchronous update is necessary. */
 }
 
 extern "C" void hmd_swap()
 {
-    if (hmd_stat())
-    {
-        /* Prepare to draw a screen-filling pair of rectangles. */
+    /* Prepare to draw a screen-filling pair of rectangles. */
 
-        glBindFramebuffer_(GL_FRAMEBUFFER, 0);
-        glViewport(0, 0, Info.HResolution, Info.VResolution);
+    glBindFramebuffer_(GL_FRAMEBUFFER, 0);
+    glViewport(0, 0, Info.HResolution, Info.VResolution);
 
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 
-        glDisable(GL_BLEND);
-        glEnableClientState(GL_VERTEX_ARRAY);
-        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisable(GL_BLEND);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
-        /* Draw the left and right eyes, distorted, to the screen. */
+    /* Draw the left and right eyes, distorted, to the screen. */
 
-        hmd_gl_draw(L_glsl.program, L_fbo.color_texture, L_vbo);
-        hmd_gl_draw(R_glsl.program, R_fbo.color_texture, R_vbo);
+    hmd_gl_draw(L_glsl.program, L_fbo.color_texture, L_vbo);
+    hmd_gl_draw(R_glsl.program, R_fbo.color_texture, R_vbo);
 
-        /* Revert the state. */
+    /* Revert the state. */
 
-        glBindBuffer_(GL_ARRAY_BUFFER, 0);
-        glUseProgram_(0);
-        glEnable(GL_BLEND);
-        glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-        glDisableClientState(GL_VERTEX_ARRAY);
-    }
+    glBindBuffer_(GL_ARRAY_BUFFER, 0);
+    glUseProgram_(0);
+    glEnable(GL_BLEND);
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
 }
 
 extern "C" void hmd_prep_left()
 {
     glBindFramebuffer_(GL_FRAMEBUFFER, L_fbo.framebuffer);
     glViewport(0, 0, Info.HResolution / 2, Info.VResolution);
-    eye = -1;
+    Params = Stereo.GetEyeRenderParams(OVR::Util::Render::StereoEye_Left);
 }
 
 extern "C" void hmd_prep_right()
 {
     glBindFramebuffer_(GL_FRAMEBUFFER, R_fbo.framebuffer);
     glViewport(0, 0, Info.HResolution / 2, Info.VResolution);
-    eye = +1;
+    Params = Stereo.GetEyeRenderParams(OVR::Util::Render::StereoEye_Right);
 }
 
 extern "C" void hmd_persp(float n, float f)
 {
     /* Projection and view matrices. */
 
-    OVR::Util::Render::StereoEyeParams params;
-
-    if (eye < 0)
-        params = Stereo.GetEyeRenderParams(OVR::Util::Render::StereoEye_Left);
-    else
-        params = Stereo.GetEyeRenderParams(OVR::Util::Render::StereoEye_Right);
-
     glMatrixMode(GL_PROJECTION);
-    hmd_gl_matrix(params.Projection);
+    hmd_gl_matrix(Params.Projection);
     glMatrixMode(GL_MODELVIEW);
-    hmd_gl_matrix(params.ViewAdjust);
+    hmd_gl_matrix(Params.ViewAdjust);
 
     /* Head orientation. */
 
-    OVR::Vector3f v;
-    float         a;
+    if (pSensor)
+    {
+        OVR::Vector3f v;
+        float         a;
 
-    Fusion.GetOrientation().GetAxisAngle(&v, &a);
-    glRotatef(OVR::RadToDegree(-a), v.x, v.y, v.z);
+        Fusion.GetOrientation().GetAxisAngle(&v, &a);
+        glRotatef(OVR::RadToDegree(-a), v.x, v.y, v.z);
+    }
 }
 
 extern "C" void hmd_ortho()
