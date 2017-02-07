be727cc303893f234fddf45e44c7459b8b27d455
acterhd <acterhd@gmail.com>
2017-02-08 01:10:03 +0800

    optimize some shaders

--- shaders-sdk/include/vertex.glsl
+++ shaders-sdk/include/vertex.glsl
@@ -3,4 +3,3 @@ layout ( std430, binding=4) buffer ssbo4 {int indics[];};
 layout ( std430, binding=5) buffer ssbo5 {Stride3f norms[];};
 layout ( std430, binding=6) buffer ssbo6 {Stride2f texcs[];};
 layout ( std430, binding=7) buffer ssbo7 {int mats[];};
-layout ( std430, binding=16) buffer ssbo16 {Stride4f vcolors[];};
