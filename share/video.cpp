c04edfa4fecbe83d8eaee24808600cde52901384
acterhd <acterhd@gmail.com>
2017-02-11 19:41:11 +0800

    using default color space

--- share/video.cpp
+++ share/video.cpp
@@ -184,11 +184,11 @@ int video_mode(int f, int w, int h)
 
     /* Require 16-bit double buffer with 16-bit depth buffer. */
 
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE,     8);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,   8);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,    8);
-    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE,   8);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,  24);
+    //SDL_GL_SetAttribute(SDL_GL_RED_SIZE,     8);
+    //SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,   8);
+    //SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,    8);
+    //SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE,   8);
+    //SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,  24);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
     /* Try to set the currently specified mode. */
