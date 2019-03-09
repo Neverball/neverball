b0ed4b87b3d0c0ea3cb20e68a20240ef6e73cde4
PennySchloss <33354707+ersohnstyne@users.noreply.github.com>
2019-03-09 18:28:54 +0100

    Show powerup in challenge mode

--- ball/st_level.c
+++ ball/st_level.c
@@ -17,12 +17,14 @@
 #include "gui.h"
 #include "set.h"
 #include "progress.h"
+#include "powerup.h"
 #include "audio.h"
 #include "config.h"
 #include "demo.h"
 
 #include "game_server.h"
 #include "game_client.h"
+#include "game_common.h"
 
 #include "st_level.h"
 #include "st_play.h"
@@ -35,8 +37,47 @@
 
 static int check_nodemo = 1;
 
+static int evalue;
+static int fvalue;
+static int svalue;
+
+enum {
+    START_LEVEL
+};
+
+static int shop_action(int tok, int val)
+{
+    switch (tok)
+    {
+    case START_LEVEL:
+        aud_play(AUD_MENU, 1.0f);
+        if (val == 3) {
+            aud_play("snd/speedifier.ogg", 1.0f);
+            svalue -= 1;
+            config_set_d(CONSUMEABLE_ACCOUNT_SPEEDIFIER, svalue);
+            init_speedifier();
+        } else if (val == 2) {
+            aud_play("snd/floatifier.ogg", 1.0f);
+            fvalue -= 1;
+            config_set_d(CONSUMEABLE_ACCOUNT_FLOATIFIER, fvalue);
+            init_floatifier();
+        } else if (val == 1) {
+            aud_play("snd/earninator.ogg", 1.0f);
+            evalue -= 1;
+            config_set_d(CONSUMEABLE_ACCOUNT_EARNINATOR, evalue);
+            init_earninator();
+        }
+        break;
+    }
+    return 1;
+}
+
 static int level_gui(void)
 {
+    evalue = config_get_d(CONSUMEABLE_ACCOUNT_EARNINATOR);
+    fvalue = config_get_d(CONSUMEABLE_ACCOUNT_FLOATIFIER);
+    svalue = config_get_d(CONSUMEABLE_ACCOUNT_SPEEDIFIER);
+
     int id, jd, kd;
 
     if ((id = gui_vstack(0)))
@@ -57,7 +98,7 @@ static int level_gui(void)
                 else
                     sprintf(lvlattr, _("Level %s"), ln);
 
-                if (curr_mode() == MODE_CHALLENGE)
+                if (curr_mode() == MODE_CHALLENGE || curr_mode() == MODE_HARDCORE)
                     sprintf(setattr, "%s: %s", set_name(curr_set()),
                             mode_to_str(MODE_CHALLENGE, 1));
                 else if (curr_mode() == MODE_STANDALONE)
@@ -78,7 +119,44 @@ static int level_gui(void)
         }
         gui_space(id);
 
-        gui_multi(id, level_msg(curr_level()), GUI_SML, gui_wht, gui_wht);
+        // REMEMBER: Put back to the newest values: curr_mode() == MODE_CHALLENGE
+        if (curr_mode() == MODE_CHALLENGE /*|| curr_mode() == MODE_HARDCORE*/) 
+        {
+            gui_label(id, _("Use special powers"), GUI_SML, gui_wht, gui_wht);
+            if ((jd = gui_harray(id)))
+            {
+                int ced, cfd, csd;
+                char pow1attr[MAXSTR], pow2attr[MAXSTR], pow3attr[MAXSTR];
+
+                sprintf(pow3attr, _("Speedifier (%i)"), svalue);
+                if ((csd = gui_varray(jd)))
+                {
+                    gui_label(csd, pow3attr, GUI_SML, svalue > 0 ? gui_grn : gui_gry, svalue > 0 ? gui_wht : gui_gry);
+                    if (svalue > 0)
+                        gui_set_state(csd, START_LEVEL, 3);
+                }
+
+                sprintf(pow2attr, _("Floatifier (%i)"), fvalue);
+                if ((cfd = gui_varray(jd)))
+                {
+                    gui_label(cfd, pow2attr, GUI_SML, fvalue > 0 ? gui_blu : gui_gry, fvalue > 0 ? gui_wht : gui_gry);
+                    if (fvalue > 0)
+                        gui_set_state(cfd, START_LEVEL, 2);
+                }
+
+                sprintf(pow1attr, _("Earninator (%i)"), evalue);
+                if ((ced = gui_varray(jd)))
+                {
+                    gui_label(ced, pow1attr, GUI_SML, evalue > 0 ? gui_red : gui_gry, evalue > 0 ? gui_wht : gui_gry);
+                    if (evalue > 0)
+                        gui_set_state(ced, START_LEVEL, 1);
+                }
+            }
+        }
+        else
+        {
+            gui_multi(id, level_msg(curr_level()), GUI_SML, gui_wht, gui_wht);
+        }
 
         gui_layout(id, 0, 0);
     }
@@ -102,12 +180,21 @@ static int level_enter(struct state *st, struct state *prev)
 
 static void level_timer(int id, float dt)
 {
+    gui_timer(id, dt);
     game_step_fade(dt);
 }
 
 static int level_click(int b, int d)
 {
-    return (b == SDL_BUTTON_LEFT && d == 1) ? goto_state(&st_play_ready) : 1;
+    // REMEMBER: Put back to the newest values: curr_mode() == MODE_NORMAL
+    if (b == SDL_BUTTON_LEFT && d == 1)
+    {
+        if (curr_mode() == MODE_NORMAL)
+        {
+            return goto_state(&st_play_ready);
+        }
+    }
+    return 1;
 }
 
 static int level_keybd(int c, int d)
@@ -129,10 +216,13 @@ static int level_buttn(int b, int d)
 {
     if (d)
     {
-        if (config_tst_d(CONFIG_JOYSTICK_BUTTON_A, b) ||
-            config_tst_d(CONFIG_JOYSTICK_BUTTON_START, b))
+        if (config_tst_d(CONFIG_JOYSTICK_BUTTON_A, b) || config_tst_d(CONFIG_JOYSTICK_BUTTON_START, b))
         {
-            return goto_state(&st_play_ready);
+            // REMEMBER: Put back to the newest values: curr_mode() == MODE_NORMAL
+            if (curr_mode() == MODE_NORMAL)
+            {
+                return goto_state(&st_play_ready);
+            }
         }
         if (config_tst_d(CONFIG_JOYSTICK_BUTTON_B, b))
         {
@@ -232,7 +322,7 @@ static int exit_enter(struct state *st, struct state *prev)
 
     if (progress_done())
         dst = &st_done;
-    else if (curr_mode() == MODE_CHALLENGE)
+    else if (curr_mode() == MODE_CHALLENGE || curr_mode() == MODE_HARDCORE)
         dst = &st_over;
     else if (curr_mode() == MODE_STANDALONE)
         dst = NULL;
@@ -265,9 +355,9 @@ struct state st_level = {
     shared_leave,
     shared_paint,
     level_timer,
-    NULL,
-    NULL,
-    NULL,
+    shared_point, // Can hover on: point
+    shared_stick, // Can hover on: stick
+    shared_angle, // Can hover on: angle
     level_click,
     level_keybd,
     level_buttn
