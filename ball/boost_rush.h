efbc1eb13c96d1f05cd907f62c878c1128c71a82
PennySchloss <33354707+ersohnstyne@users.noreply.github.com>
2019-03-25 11:58:27 +0100

    Boost rush installed
    Same as New Super Mario Bros U

---
+++ ball/boost_rush.h
@@ -0,0 +1,12 @@
+#ifndef BOOST_RUSH_H
+#define BOOST_RUSH_H
+
+void collect_coins(int);
+void increase_speed(void);
+void substract_speed(void);
+void speed_timer(float);
+
+float get_speed_indicator(void);
+int is_sonic(void);
+
+#endif
