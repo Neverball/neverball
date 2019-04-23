3c4511fc67a49d91d444f51a9256436d8657dc7f
PennySchloss <33354707+ersohnstyne@users.noreply.github.com>
2019-04-23 10:27:02 +0200

    Fixed that methods doesn't correct for Checkpoints

--- ball/checkpoints.h
+++ ball/checkpoints.h
@@ -1,11 +1,16 @@
 #ifndef CHECKPOINTS_H
 #define CHECKPOINTS_H
 
+extern int checkpoints_busy;
+
 extern int last_active;
 
 extern float last_position_x;
 extern float last_position_y;
 extern float last_position_z;
+extern float last_orientation_x;
+extern float last_orientation_y;
+extern float last_orientation_z;
 extern float last_r;
 
 extern float last_time;
@@ -16,11 +21,18 @@ extern int last_timer_down;
 
 /*---------------------------------------------------------------------------*/
 
+void checkpoints_increment_server_step(float dt);
+void checkpoints_save_game_server_step(void);
+
+void checkpoints_respawn(void);
+void checkpoints_respawn_done(void);
+
 void checkpoints_stop(void);
+
 void set_active_checkpoint(int);
 
-void set_last_transform(float, float, float, float);
-void set_last_data(float, int, int);
+void set_last_transform(float[], float);
+void set_last_data(float, int);
 void set_last_timer_down(int);
 
 #endif
