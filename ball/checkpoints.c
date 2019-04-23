5b939400ac09d30bf9b660d0c27954d1c6c5ac99
PennySchloss <33354707+ersohnstyne@users.noreply.github.com>
2019-04-23 10:27:05 +0200

    Fixed that methods doesn't correct for Checkpoints

--- ball/checkpoints.c
+++ ball/checkpoints.c
@@ -18,15 +18,27 @@
  */
 #include "checkpoints.h"
 
+#include "game_server.h"
+
 /*---------------------------------------------------------------------------*/
+
+int checkpoints_busy;
+
+float current_game_step;
+float saved_game_step;
 
 int last_active;
 
+/* Transforms */
 float last_position_x;
 float last_position_y;
 float last_position_z;
+float last_orientation_x;
+float last_orientation_y;
+float last_orientation_z;
 float last_r;
 
+/* Level datas */
 float last_time;
 int last_coins;
 int last_goal;
@@ -35,8 +47,32 @@ int last_timer_down;
 
 /*---------------------------------------------------------------------------*/
 
+void checkpoints_increment_server_step(float dt)
+{
+    current_game_step += dt;
+}
+
+void checkpoints_save_game_server_step(void)
+{
+    saved_game_step = current_game_step;
+}
+
+void checkpoints_respawn(void)
+{
+    checkpoints_busy = 1;
+    game_server_step(saved_game_step);
+}
+
+void checkpoints_respawn_done(void)
+{
+    checkpoints_busy = 0;
+}
+
 void checkpoints_stop(void)
 {
+    current_game_step = 0;
+    saved_game_step = 0;
+
     last_active = 0;
 
     last_position_x = 0;
@@ -56,21 +92,20 @@ void set_active_checkpoint(int new_active)
     last_active = new_active;
 }
 
-void set_last_transform(float x, float y, float z, float radius)
+void set_last_transform(float pos[3], float radius)
 {
     /* Set the last transform */
-    last_position_x = x;
-    last_position_y = y;
-    last_position_z = z;
+    last_position_x = pos[0];
+    last_position_y = pos[1];
+    last_position_z = pos[2];
     last_r = radius;
 }
 
-void set_last_data(float time, int coins, int goal)
+void set_last_data(float time, int coins)
 {
     /* Set the last level data */
     last_time = time;
     last_coins = coins;
-    last_goal = goal;
 }
 
 void set_last_timer_down(int enabled)
