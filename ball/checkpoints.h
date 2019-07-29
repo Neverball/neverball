5cd626f6d96d49cf910c60f2e19a0bf2961615f7
PennySchloss <33354707+ersohnstyne@users.noreply.github.com>
2019-07-29 16:59:01 +0200

    Big fixes in checkpoints

--- ball/checkpoints.h
+++ ball/checkpoints.h
@@ -2,8 +2,10 @@
 #define CHECKPOINTS_H
 
 extern int checkpoints_busy;
-
 extern int last_active;
+
+extern struct s_vary last_vary;
+extern struct game_view last_view;
 
 extern float last_position_x;
 extern float last_position_y;
@@ -21,15 +23,12 @@ extern int last_timer_down;
 
 /*---------------------------------------------------------------------------*/
 
-void checkpoints_increment_server_step(float dt);
-void checkpoints_save_game_server_step(void);
+void checkpoints_save_spawnpoint(struct s_vary, struct game_view);
 
 void checkpoints_respawn(void);
 void checkpoints_respawn_done(void);
 
 void checkpoints_stop(void);
-
-void set_active_checkpoint(int);
 
 void set_last_transform(float[], float);
 void set_last_data(float, int);
