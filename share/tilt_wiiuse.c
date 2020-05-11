ca72ffb1fcedc58ee5ee5948461ddffcdad1b160
nico1965_nico@yahoo.fr <nico1965_nico@yahoo.fr>
2020-05-11 08:58:13 +0200

    remove tabs

--- share/tilt_wiiuse.c
+++ share/tilt_wiiuse.c
@@ -45,113 +45,113 @@ static SDL_Thread       *thread = NULL;
 
 static int tilt_thread(void *data)
 {
-	wiimote** wiimotes = NULL;
-	int found = 0;
-	int connected = 0;
-	struct wiimote_t* wm = NULL;
-	int i = 0;
-	float pitch_sensitivity = 1.;
-	float roll_sensitivity = 1.;
-	float smooth_alpha = 0.5;
+    wiimote** wiimotes = NULL;
+    int found = 0;
+    int connected = 0;
+    struct wiimote_t* wm = NULL;
+    int i = 0;
+    float pitch_sensitivity = 1.;
+    float roll_sensitivity = 1.;
+    float smooth_alpha = 0.5;
 
-	wiimotes = wiiuse_init(NB_WIIMOTES);
+    wiimotes = wiiuse_init(NB_WIIMOTES);
 
-	found = wiiuse_find(wiimotes, NB_WIIMOTES, 5);
-	if (!found) {
-		log_printf("No wiimotes found.\n");
-		return 0;
-	}
+    found = wiiuse_find(wiimotes, NB_WIIMOTES, 5);
+    if (!found) {
+        log_printf("No wiimotes found.\n");
+        return 0;
+    }
 
-	connected = wiiuse_connect(wiimotes, NB_WIIMOTES);
-	if (connected) {
-		log_printf("Connected to %i wiimotes (of %i found).\n", connected, found);
-	} else {
-		log_printf("Failed to connect to any wiimote.\n");
-		return 0;
-	}
-	wm = wiimotes[0];
+    connected = wiiuse_connect(wiimotes, NB_WIIMOTES);
+    if (connected) {
+        log_printf("Connected to %i wiimotes (of %i found).\n", connected, found);
+    } else {
+        log_printf("Failed to connect to any wiimote.\n");
+        return 0;
+    }
+    wm = wiimotes[0];
 
-	pitch_sensitivity = ((float)config_get_d(CONFIG_WIIMOTE_PITCH_SENSITIVITY) / 100);
-	pitch_sensitivity *= ((config_get_d(CONFIG_WIIMOTE_INVERT_PITCH)) ? -1 : 1);
-	roll_sensitivity = ((float)config_get_d(CONFIG_WIIMOTE_ROLL_SENSITIVITY) / 100);
-	roll_sensitivity *= ((config_get_d(CONFIG_WIIMOTE_INVERT_ROLL)) ? -1 : 1);
-	smooth_alpha = (float)config_get_d(CONFIG_WIIMOTE_SMOOTH_ALPHA) / 100;
+    pitch_sensitivity = ((float)config_get_d(CONFIG_WIIMOTE_PITCH_SENSITIVITY) / 100);
+    pitch_sensitivity *= ((config_get_d(CONFIG_WIIMOTE_INVERT_PITCH)) ? -1 : 1);
+    roll_sensitivity = ((float)config_get_d(CONFIG_WIIMOTE_ROLL_SENSITIVITY) / 100);
+    roll_sensitivity *= ((config_get_d(CONFIG_WIIMOTE_INVERT_ROLL)) ? -1 : 1);
+    smooth_alpha = (float)config_get_d(CONFIG_WIIMOTE_SMOOTH_ALPHA) / 100;
 
-	wiiuse_motion_sensing(wm, 1);
-	wiiuse_set_smooth_alpha(wm, smooth_alpha);
-	wiiuse_set_leds(wm, WIIMOTE_LED_1);
+    wiiuse_motion_sensing(wm, 1);
+    wiiuse_set_smooth_alpha(wm, smooth_alpha);
+    wiiuse_set_leds(wm, WIIMOTE_LED_1);
 
     SDL_mutexP(mutex);
     current_state.status = 1;
     SDL_mutexV(mutex);
 
-	while (mutex && current_state.status && WIIMOTE_IS_CONNECTED(wm)) {
-		if (wiiuse_poll(wiimotes, NB_WIIMOTES)) {
-			switch (wm->event) {
-				case WIIUSE_EVENT:
-					SDL_mutexP(mutex);
-					for (i = 0; wiiUseButtons[i][0] != 0; i++) {
-						current_state.buttons[i] = IS_PRESSED(wm, wiiUseButtons[i][0]);
-					}
-					if (WIIUSE_USING_ACC(wm)) {
-						if (config_get_d(CONFIG_WIIMOTE_HOLD_SIDEWAYS)) {
-							current_state.z = -wm->orient.pitch * pitch_sensitivity;
-						    current_state.x = wm->orient.roll * roll_sensitivity;
-						}
-						else {
-							current_state.x = wm->orient.pitch * pitch_sensitivity;
-						    current_state.z = wm->orient.roll * roll_sensitivity;
-						}
-					}
+    while (mutex && current_state.status && WIIMOTE_IS_CONNECTED(wm)) {
+        if (wiiuse_poll(wiimotes, NB_WIIMOTES)) {
+            switch (wm->event) {
+                case WIIUSE_EVENT:
+                    SDL_mutexP(mutex);
+                    for (i = 0; wiiUseButtons[i][0] != 0; i++) {
+                        current_state.buttons[i] = IS_PRESSED(wm, wiiUseButtons[i][0]);
+                    }
+                    if (WIIUSE_USING_ACC(wm)) {
+                        if (config_get_d(CONFIG_WIIMOTE_HOLD_SIDEWAYS)) {
+                            current_state.z = -wm->orient.pitch * pitch_sensitivity;
+                            current_state.x = wm->orient.roll * roll_sensitivity;
+                        }
+                        else {
+                            current_state.x = wm->orient.pitch * pitch_sensitivity;
+                            current_state.z = wm->orient.roll * roll_sensitivity;
+                        }
+                    }
                     SDL_mutexV(mutex);
                     break;
-				default:
-					break;
+                default:
+                    break;
             }
         }
     }
 
-	wiiuse_set_leds(wm, 0);
-	wiiuse_cleanup(wiimotes, NB_WIIMOTES);
+    wiiuse_set_leds(wm, 0);
+    wiiuse_cleanup(wiimotes, NB_WIIMOTES);
     return 0;
 }
 
 void tilt_init(void)
 {
-	int wiiUseButtonsHoldNormal[NB_WIIMOTE_BUTTONS][2] = {
-		{WIIMOTE_BUTTON_A       , CONFIG_JOYSTICK_BUTTON_A},
-		{WIIMOTE_BUTTON_B       , CONFIG_JOYSTICK_BUTTON_B},
-		{WIIMOTE_BUTTON_MINUS   , CONFIG_JOYSTICK_BUTTON_L1},
-		{WIIMOTE_BUTTON_PLUS    , CONFIG_JOYSTICK_BUTTON_R1},
-		{WIIMOTE_BUTTON_ONE     , CONFIG_JOYSTICK_BUTTON_X},
-		{WIIMOTE_BUTTON_TWO     , CONFIG_JOYSTICK_BUTTON_Y},
-		{WIIMOTE_BUTTON_HOME    , CONFIG_JOYSTICK_BUTTON_START},
-		{WIIMOTE_BUTTON_LEFT    , CONFIG_JOYSTICK_DPAD_L},
-		{WIIMOTE_BUTTON_RIGHT   , CONFIG_JOYSTICK_DPAD_R},
-		{WIIMOTE_BUTTON_DOWN    , CONFIG_JOYSTICK_DPAD_D},
-		{WIIMOTE_BUTTON_UP      , CONFIG_JOYSTICK_DPAD_U},
-		{0, 0}
-	};
-	int wiiUseButtonsHoldSideways[NB_WIIMOTE_BUTTONS][2] = {
-		{WIIMOTE_BUTTON_A       , CONFIG_JOYSTICK_BUTTON_A},
-		{WIIMOTE_BUTTON_B       , CONFIG_JOYSTICK_BUTTON_B},
-		{WIIMOTE_BUTTON_MINUS   , CONFIG_JOYSTICK_BUTTON_X},
-		{WIIMOTE_BUTTON_PLUS    , CONFIG_JOYSTICK_BUTTON_Y},
-		{WIIMOTE_BUTTON_ONE     , CONFIG_JOYSTICK_BUTTON_L1},
-		{WIIMOTE_BUTTON_TWO     , CONFIG_JOYSTICK_BUTTON_R1},
-		{WIIMOTE_BUTTON_HOME    , CONFIG_JOYSTICK_BUTTON_START},
-		{WIIMOTE_BUTTON_LEFT    , CONFIG_JOYSTICK_DPAD_D},
-		{WIIMOTE_BUTTON_RIGHT   , CONFIG_JOYSTICK_DPAD_U},
-		{WIIMOTE_BUTTON_DOWN    , CONFIG_JOYSTICK_DPAD_R},
-		{WIIMOTE_BUTTON_UP      , CONFIG_JOYSTICK_DPAD_L},
-		{0, 0}
-	};
+    int wiiUseButtonsHoldNormal[NB_WIIMOTE_BUTTONS][2] = {
+        {WIIMOTE_BUTTON_A       , CONFIG_JOYSTICK_BUTTON_A},
+        {WIIMOTE_BUTTON_B       , CONFIG_JOYSTICK_BUTTON_B},
+        {WIIMOTE_BUTTON_MINUS   , CONFIG_JOYSTICK_BUTTON_L1},
+        {WIIMOTE_BUTTON_PLUS    , CONFIG_JOYSTICK_BUTTON_R1},
+        {WIIMOTE_BUTTON_ONE     , CONFIG_JOYSTICK_BUTTON_X},
+        {WIIMOTE_BUTTON_TWO     , CONFIG_JOYSTICK_BUTTON_Y},
+        {WIIMOTE_BUTTON_HOME    , CONFIG_JOYSTICK_BUTTON_START},
+        {WIIMOTE_BUTTON_LEFT    , CONFIG_JOYSTICK_DPAD_L},
+        {WIIMOTE_BUTTON_RIGHT   , CONFIG_JOYSTICK_DPAD_R},
+        {WIIMOTE_BUTTON_DOWN    , CONFIG_JOYSTICK_DPAD_D},
+        {WIIMOTE_BUTTON_UP      , CONFIG_JOYSTICK_DPAD_U},
+        {0, 0}
+    };
+    int wiiUseButtonsHoldSideways[NB_WIIMOTE_BUTTONS][2] = {
+        {WIIMOTE_BUTTON_A       , CONFIG_JOYSTICK_BUTTON_A},
+        {WIIMOTE_BUTTON_B       , CONFIG_JOYSTICK_BUTTON_B},
+        {WIIMOTE_BUTTON_MINUS   , CONFIG_JOYSTICK_BUTTON_X},
+        {WIIMOTE_BUTTON_PLUS    , CONFIG_JOYSTICK_BUTTON_Y},
+        {WIIMOTE_BUTTON_ONE     , CONFIG_JOYSTICK_BUTTON_L1},
+        {WIIMOTE_BUTTON_TWO     , CONFIG_JOYSTICK_BUTTON_R1},
+        {WIIMOTE_BUTTON_HOME    , CONFIG_JOYSTICK_BUTTON_START},
+        {WIIMOTE_BUTTON_LEFT    , CONFIG_JOYSTICK_DPAD_D},
+        {WIIMOTE_BUTTON_RIGHT   , CONFIG_JOYSTICK_DPAD_U},
+        {WIIMOTE_BUTTON_DOWN    , CONFIG_JOYSTICK_DPAD_R},
+        {WIIMOTE_BUTTON_UP      , CONFIG_JOYSTICK_DPAD_L},
+        {0, 0}
+    };
 
-	memset(&current_state, 0, sizeof (struct tilt_state));
-	if  (config_get_d(CONFIG_WIIMOTE_HOLD_SIDEWAYS))
-		memcpy(wiiUseButtons, wiiUseButtonsHoldSideways, sizeof(wiiUseButtons));
-	else
-		memcpy(wiiUseButtons, wiiUseButtonsHoldNormal, sizeof(wiiUseButtons));
+    memset(&current_state, 0, sizeof (struct tilt_state));
+    if  (config_get_d(CONFIG_WIIMOTE_HOLD_SIDEWAYS))
+        memcpy(wiiUseButtons, wiiUseButtonsHoldSideways, sizeof(wiiUseButtons));
+    else
+        memcpy(wiiUseButtons, wiiUseButtonsHoldNormal, sizeof(wiiUseButtons));
 
     mutex  = SDL_CreateMutex();
     thread = SDL_CreateThread(tilt_thread, "", NULL);
@@ -178,19 +178,19 @@ void tilt_free(void)
 
 int tilt_get_button(int *b, int *s)
 {
-	int i = 0;
+    int i = 0;
 
     if (mutex && current_state.status)
     {
-    	SDL_mutexP(mutex);
-		for (i = 0; wiiUseButtons[i][0] != 0; i++) {
-			if (current_state.buttons[i] != polled_state.buttons[i]) {
-	            *b = config_get_d(wiiUseButtons[i][1]);
-	            *s = current_state.buttons[i];
-	            polled_state.buttons[i] = current_state.buttons[i];
-	            break;
-			}
-		}
+        SDL_mutexP(mutex);
+        for (i = 0; wiiUseButtons[i][0] != 0; i++) {
+            if (current_state.buttons[i] != polled_state.buttons[i]) {
+                *b = config_get_d(wiiUseButtons[i][1]);
+                *s = current_state.buttons[i];
+                polled_state.buttons[i] = current_state.buttons[i];
+                break;
+            }
+        }
         SDL_mutexV(mutex);
     }
 
