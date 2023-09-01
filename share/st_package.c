696942239293ba3eb7a43e6566622b865f303fde
ErsohnStyneEntertainment <ersohn.styne@gmail.com>
2023-09-01 10:55:33 +0200

    Update st_package.c
    Added advanced package manager support

--- share/st_package.c
+++ share/st_package.c
@@ -26,10 +26,14 @@
 
 /*---------------------------------------------------------------------------*/
 
+struct state st_package_manage;
+
 #define PACKAGE_STEP 4
 
-static int total = 0;
-static int first = 0;
+static int package_manual_hotreload = 0;
+
+static int total    = 0;
+static int first    = 0;
 static int selected = 0;
 
 static int shot_id;
@@ -40,25 +44,28 @@ static int install_id;
 static int install_status_id;
 static int install_label_id;
 
-static int do_init = 1;
+static int category_id = PACKAGE_CATEGORY_LEVELSET;
+static int do_init     = 1;
+static int do_download = 0;
 
 static struct state *package_back;
 
 static int  button_ids[PACKAGE_STEP] = {0};
 static int *status_ids = NULL;
-static int *name_ids = NULL;
+static int *name_ids   = NULL;
 
 enum
 {
     PACKAGE_INSTALL = GUI_LAST,
     PACKAGE_UNINSTALL,
-    PACKAGE_SELECT
+    PACKAGE_SELECT,
+    PACKAGE_CHANGEGROUP,
 };
 
 struct download_info
 {
     char *package_id;
-    char label[32];
+    char  label[32];
 };
 
 static struct download_info *create_download_info(const char *package_id)
@@ -88,7 +95,7 @@ static void free_download_info(struct download_info *dli)
 
 static void download_progress(void *data1, void *data2)
 {
-    struct download_info *dli = data1;
+    struct download_info  *dli = data1;
     struct fetch_progress *pr = data2;
 
     if (dli)
@@ -105,7 +112,8 @@ static void download_progress(void *data1, void *data2)
                 char label[32] = GUI_ELLIPSIS;
 
                 if (pr->total > 0)
-                    sprintf(label, "%3d%%", (int) (pr->now * 100.0 / pr->total) % 1000);
+                    sprintf(label, "%3d%%",
+                                   (int) (pr->now * 100.0 / pr->total) % 1000);
 
                 /* Compare against current label so we're not calling GL constantly. */
                 /* TODO: just do this in gui_set_label. */
@@ -125,7 +133,7 @@ static void package_select(int);
 static void download_done(void *data1, void *data2)
 {
     struct download_info *dli = data1;
-    struct fetch_done *dn = data2;
+    struct fetch_done    *dn = data2;
 
     if (dli)
     {
@@ -163,11 +171,33 @@ static void download_done(void *data1, void *data2)
     }
 }
 
+static void package_start_download(int id)
+{
+    do_download = 0;
+
+    struct fetch_callback callback = {0};
+
+    callback.progress = download_progress;
+    callback.done     = download_done;
+    callback.data     = create_download_info(package_get_id(id));
+
+    if (!package_fetch(id, callback, category_id))
+    {
+        free_download_info(callback.data);
+        callback.data = NULL;
+    }
+    else if (status_ids && status_ids[id])
+    {
+        gui_set_label(status_ids[id], GUI_ELLIPSIS);
+        gui_set_color(status_ids[id], gui_grn, gui_grn);
+    }
+}
+
 static int package_action(int tok, int val)
 {
     enum package_status status;
 
-    audio_play(AUD_MENU, 1.0f);
+    audio_play(AUD_MENU, 1.f);
 
     switch (tok)
     {
@@ -176,7 +206,6 @@ static int package_action(int tok, int val)
         break;
 
     case GUI_PREV:
-
         first -= PACKAGE_STEP;
 
         do_init = 0;
@@ -185,14 +214,13 @@ static int package_action(int tok, int val)
         break;
 
     case GUI_NEXT:
-
         first += PACKAGE_STEP;
 
         do_init = 0;
         return goto_state(&st_package);
 
         break;
-    
+
     case PACKAGE_SELECT:
         package_select(val);
         break;
@@ -201,39 +229,22 @@ static int package_action(int tok, int val)
     case PACKAGE_INSTALL:
         status = package_get_status(selected);
 
-        if (status == PACKAGE_INSTALLED)
+        if (status == PACKAGE_INSTALLED ||
+            status == PACKAGE_PARTIAL)
+            return goto_state(&st_package_manage);
+        else if (status == PACKAGE_AVAILABLE ||
+                 status == PACKAGE_ERROR)
         {
-            // TODO: unload from VFS, remove from installed packages, and delete ZIP.
-            return 1;
-        }
-        else if (status == PACKAGE_AVAILABLE || status == PACKAGE_PARTIAL || status == PACKAGE_ERROR)
-        {
-            struct fetch_callback callback = { 0 };
-
-            callback.progress = download_progress;
-            callback.done = download_done;
-            callback.data = create_download_info(package_get_id(selected));
-
-            if (!package_fetch(selected, callback))
-            {
-                free_download_info(callback.data);
-                callback.data = NULL;
-            }
-            else
-            {
-                if (status_ids && status_ids[selected])
-                {
-                    gui_set_label(status_ids[selected], GUI_ELLIPSIS);
-                    gui_set_color(status_ids[selected], gui_grn, gui_grn);
-                }
-            }
+            package_start_download(selected);
 
             return 1;
         }
         else if (status == PACKAGE_DOWNLOADING)
-        {
             return 1;
-        }
+        break;
+
+    case PACKAGE_CHANGEGROUP:
+        category_id = val;
         break;
     }
 
@@ -260,17 +271,17 @@ static int gui_package_button(int id, int pi)
         if (status_ids)
             status_ids[pi] = status_id;
 
-        name_id = gui_label(jd, "JKLMNOPQRSTUVWXYZ", GUI_SML, gui_wht, gui_wht);
+        name_id = gui_label(jd, "XXXXXXXXXXXXXXXXX", GUI_SML, gui_wht, gui_wht);
 
         gui_set_trunc(name_id, TRUNC_TAIL);
         gui_set_label(name_id, package_get_name(pi));
-        gui_set_fill(name_id);
+        gui_set_fill (name_id);
 
         if (name_ids)
             name_ids[pi] = name_id;
 
         gui_set_state(jd, PACKAGE_SELECT, pi);
-        gui_set_rect(jd, GUI_ALL);
+        gui_set_rect (jd, GUI_ALL);
     }
 
     return jd;
@@ -288,7 +299,6 @@ static int package_gui(void)
 {
     int w = video.device_w;
     int h = video.device_h;
-
     int id, jd, kd;
 
     int i;
@@ -297,24 +307,21 @@ static int package_gui(void)
     {
         if ((id = gui_vstack(0)))
         {
-            gui_label(id, _("No packages available"), GUI_SML, 0, 0);
-            gui_space(id);
-            gui_state(id, _("Back"), GUI_SML, GUI_BACK, 0);
+            gui_label (id, _("No packages available"), GUI_SML, 0, 0);
+            gui_space (id);
+            gui_state (id, _("Back"), GUI_SML, GUI_BACK, 0);
             gui_layout(id, 0, 0);
         }
-
         return id;
     }
-
     if ((id = gui_vstack(0)))
     {
         if ((jd = gui_hstack(id)))
         {
-            gui_label(jd, _("Packages"), GUI_SML, gui_yel, gui_red);
+            gui_label (jd, _("Packages"), GUI_SML, 0, 0);
             gui_filler(jd);
-            gui_navig(jd, total, first, PACKAGE_STEP);
+            gui_navig (jd, total, first, PACKAGE_STEP);
         }
-
         gui_space(id);
 
         if ((jd = gui_harray(id)))
@@ -339,7 +346,8 @@ static int package_gui(void)
 
         if ((jd = gui_vstack(id)))
         {
-            title_id = gui_label(jd, package_get_name(first), GUI_SML, gui_yel, gui_wht);
+            title_id = gui_label(jd, package_get_name(first),
+                                     GUI_SML, gui_yel, gui_wht);
 
             gui_space(jd);
 
@@ -354,8 +362,10 @@ static int package_gui(void)
         {
             if ((kd = gui_hstack(jd)))
             {
-                install_status_id = gui_label(kd, GUI_ARROW_DN, GUI_SML, gui_grn, gui_grn);
-                install_label_id = gui_label(kd, _("Install"), GUI_SML, gui_wht, gui_wht);
+                install_status_id = gui_label(kd, GUI_ARROW_DN,
+                                                  GUI_SML, gui_grn, gui_grn);
+                install_label_id  = gui_label(kd, _("Install"),
+                                                  GUI_SML, gui_wht, gui_wht);
 
                 gui_set_rect(kd, GUI_ALL);
                 gui_set_state(kd, PACKAGE_INSTALL, 0);
@@ -365,7 +375,7 @@ static int package_gui(void)
 
             gui_filler(jd);
 
-            type_id = gui_label(jd, "ABCDEFG", GUI_SML, gui_yel, gui_wht);
+            type_id = gui_label(jd, "XXXXXXX", GUI_SML, gui_yel, gui_wht);
         }
 
         gui_layout(id, 0, 0);
@@ -384,9 +394,9 @@ static void package_select(int pi)
     selected = pi;
     gui_set_hilite(button_ids[selected % PACKAGE_STEP], 1);
 
-    gui_set_image(shot_id, package_get_shot_filename(pi));
-    gui_set_multi(desc_id, package_get_desc(pi));
-    gui_set_label(type_id, package_get_formatted_type(pi));
+    gui_set_image(shot_id,  package_get_shot_filename(pi));
+    gui_set_multi(desc_id,  package_get_desc(pi));
+    gui_set_label(type_id,  package_get_formatted_type(pi));
     gui_set_label(title_id, package_get_name(pi));
 
     if (package_get_status(pi) == PACKAGE_INSTALLED)
@@ -407,14 +417,19 @@ static int package_enter(struct state *st, struct state *prev)
 
     back_init("back/gui.png");
 
-    if (do_init)
+    if (do_init || package_manual_hotreload)
     {
         package_back = prev;
+
+        if (package_manual_hotreload)
+            first = 0;
 
         total = package_count();
         first = MIN(first, (total - 1) - ((total - 1) % PACKAGE_STEP));
 
         audio_music_fade_to(0.5f, "bgm/inter.ogg");
+
+        package_manual_hotreload = 0;
     }
     else do_init = 1;
 
@@ -467,6 +482,14 @@ void package_paint(int id, float st)
     gui_paint(id);
 }
 
+void package_timer(int id, float dt)
+{
+    if (do_download && time_state() > 0.5f)
+        package_start_download(selected);
+
+    gui_timer(id, dt);
+}
+
 static void package_point(int id, int x, int y, int dx, int dy)
 {
     int jd = gui_point(id, x, y);
@@ -483,11 +506,180 @@ static void package_stick(int id, int a, float v, int bump)
         gui_pulse(jd, 1.2f);
 }
 
+static int package_keybd(int c, int d)
+{
+    if (d)
+    {
+        if (c == KEY_EXIT)
+            return package_action(GUI_BACK, 0);
+        else if (c == KEY_LOOKAROUND)
+        {
+#ifndef NDEBUG
+            log_printf("Attempt to reload package list.\n");
+#endif
+            package_manual_hotreload = 1;
+            return goto_state(&st_package);
+        }
+    }
+
+    return 1;
+}
+
+/*---------------------------------------------------------------------------*/
+
+/*
+ * HACK: This is how Mojang UI's file manager works.
+ * - ersohnstyne
+ */
+
+enum
+{
+    PACKAGE_MANAGE_EXPLORER = GUI_LAST,
+    PACKAGE_MANAGE_UPDATE,
+    PACKAGE_MANAGE_DELETE
+};
+
+enum package_confirm_action
+{
+    PACKAGE_CONFIRM_NONE = 0,
+
+    PACKAGE_CONFIRM_DELETE,
+    PACKAGE_CONFIRM_RENAME,
+
+    PACKAGE_CONFIRM_MAX
+};
+
+static enum package_confirm_action curr_confirm_action = PACKAGE_CONFIRM_NONE;
+
+int package_manage_action(int tok, int val)
+{
+    audio_play(AUD_MENU, 1.f);
+    
+    switch (curr_confirm_action)
+    {
+        case PACKAGE_CONFIRM_DELETE:
+        {
+            switch (tok)
+            {
+            case PACKAGE_MANAGE_DELETE:
+                curr_confirm_action = PACKAGE_CONFIRM_NONE;
+                fs_remove(package_get_files(selected));
+                return goto_state(&st_package);
+
+            case GUI_BACK:
+                curr_confirm_action = PACKAGE_CONFIRM_NONE;
+                return goto_state(&st_package);
+            }
+        }
+        break;
+
+        default:
+        {
+            switch (tok)
+            {
+            case PACKAGE_MANAGE_UPDATE:
+                selected = val;
+                do_download = 1;
+                return goto_state(&st_package);
+
+            case PACKAGE_MANAGE_DELETE:
+                curr_confirm_action = PACKAGE_CONFIRM_DELETE;
+                return goto_state(&st_package_manage);
+
+            case GUI_BACK:
+                return goto_state(&st_package);
+            }
+        }
+        break;
+    }
+
+    return 1;
+}
+
+int package_manage_delete_gui(void)
+{
+    int id, jd;
+
+    if ((id = gui_vstack(0)))
+    {
+        char desc[MAXSTR];
+        sprintf(desc, _("Seriously, this action cannot be undone!:\\%s"),
+                      package_get_name(selected));
+
+        gui_title_header(id, _("Delete package?"), GUI_MED, gui_red, gui_red);
+
+        gui_space(id);
+
+        gui_multi(id, desc, GUI_SML, gui_wht, gui_wht);
+
+        gui_space(id);
+
+        if ((jd = gui_harray(id)))
+        {
+            gui_state(jd, _("Cancel"), GUI_SML, GUI_BACK, 0);
+            gui_start(jd, _("Delete"), GUI_SML, PACKAGE_MANAGE_DELETE, selected);
+        }
+    }
+
+    return id;
+}
+
+int package_manage_gui(void)
+{
+    int id, btn_id;
+
+    if ((id = gui_vstack(0)))
+    {
+        char desc[MAXSTR];
+        sprintf(desc, _("Installed package path:\\%s"),
+                         package_get_files(selected));
+
+        gui_multi(id, desc, GUI_SML, gui_wht, gui_wht);
+
+        gui_space(id);
+
+        if ((btn_id = gui_state(id, _("Update"),
+                                    GUI_SML, PACKAGE_MANAGE_UPDATE, selected)))
+            gui_set_color(btn_id, gui_wht, gui_wht);
+        if ((btn_id = gui_state(id, _("Delete"),
+                                    GUI_SML, PACKAGE_MANAGE_DELETE, selected)))
+            gui_set_color(btn_id, gui_red, gui_red);
+    }
+
+    return id;
+}
+
+int package_manage_enter(struct state *st, struct state *prev)
+{
+    common_init(package_manage_action);
+
+    return curr_confirm_action == PACKAGE_CONFIRM_DELETE ?
+           package_manage_delete_gui() : package_manage_gui();
+}
+
+static void package_manage_leave(struct state *st, struct state *next, int id)
+{
+    gui_delete(id);
+}
+
 /*---------------------------------------------------------------------------*/
 
 struct state st_package = {
     package_enter,
     package_leave,
+    package_paint,
+    package_timer,
+    package_point,
+    package_stick,
+    NULL,
+    common_click,
+    package_keybd,
+    common_buttn
+};
+
+struct state st_package_manage = {
+    package_manage_enter,
+    package_manage_leave,
     package_paint,
     common_timer,
     package_point,
