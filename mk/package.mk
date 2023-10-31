b495ca40567396a680604fe44920b9698a449674
Jānis Rūcis <parasti@gmail.com>
2023-10-31 21:49:31 +0000

    Restore ZIP determinism

--- mk/package.mk
+++ mk/package.mk
@@ -32,6 +32,7 @@ LOCAL_PATHS := $(ASSET_PATHS:%=$(DATA_DIR)/%) # prefix with ./data/
 LOCAL_FILES := $(shell find $(LOCAL_PATHS) -type f) # find files to hash
 ASSET_HASH := $(shell sh scripts/hash-assets.sh $(LOCAL_PATHS))
 PACKAGE_ZIP := $(PACKAGE_ID)-$(ASSET_HASH).zip
+PACKAGE_TMP := /tmp/$(PACKAGE_ID)
 PACKAGE_MANIFEST := $(PACKAGE_ID).manifest.txt
 
 all: output-dir package manifest
@@ -40,7 +41,18 @@ output-dir:
 	mkdir -p $(OUTPUT_DIR)
 
 package: output-dir
-	cd $(DATA_DIR) && zip -X -q -r $(OUTPUT_DIR)/$(PACKAGE_ZIP) $(ASSET_PATHS) -x $(PACKAGE_EXCLUDE)
+	cd $(DATA_DIR) && \
+	zip -X -q -r $(OUTPUT_DIR)/$(PACKAGE_ZIP) $(ASSET_PATHS) -x $(PACKAGE_EXCLUDE) && \
+	rm -rf "$(PACKAGE_TMP)" && \
+	mkdir "$(PACKAGE_TMP)" && \
+	( \
+		cd "$(PACKAGE_TMP)" && \
+		unzip -q $(OUTPUT_DIR)/$(PACKAGE_ZIP) && \
+		find . -execdir touch -t 200305010000 {} \; && \
+		rm $(OUTPUT_DIR)/$(PACKAGE_ZIP) && \
+		zip -X -q -r $(OUTPUT_DIR)/$(PACKAGE_ZIP) . \
+	) && \
+	rm -rf "$(PACKAGE_TMP)"
 
 manifest: output-dir package
 	printf 'package %s\nfilename %s\nsize %s\n' $(PACKAGE_ID) $(PACKAGE_ZIP) $(shell du -b $(OUTPUT_DIR)/$(PACKAGE_ZIP) | cut -f1) > $(OUTPUT_DIR)/$(PACKAGE_MANIFEST)
