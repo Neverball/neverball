1db97e6219e8892f4f48558e28271d31bd87b7e2
Jānis Rūcis <parasti@gmail.com>
2024-01-05 23:49:20 +0200

    .devcontainer: tweak

--- .devcontainer/install-gl4es.sh
+++ .devcontainer/install-gl4es.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-[ "$GL4ES_COMMIT"  ] || exit 1
+[ "$GL4ES_COMMIT" ] || exit 1
 
-git clone https://github.com/ptitSeb/gl4es.git "${GL4ES_DIR}" &&
+git clone --no-tags --single-branch https://github.com/ptitSeb/gl4es.git "${GL4ES_DIR}" &&
 cd "${GL4ES_DIR}" && {
     git checkout $GL4ES_COMMIT &&
     mkdir build &&
     cd build &&
     emcmake cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DNOX11=ON -DNOEGL=ON -DSTATICLIB=ON &&
-    make &&
-    chown -R emscripten:emscripten .
+    make
 }
