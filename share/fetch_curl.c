18899908df201abdf1fbd9d3e446b64559472eb1
Jānis Rūcis <parasti@gmail.com>
2024-06-15 20:41:07 +0000

    Add some license headers

--- share/fetch_curl.c
+++ share/fetch_curl.c
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2021-2024 Jānis Rūcis
+ *
+ * NEVERBALL is  free software; you can redistribute  it and/or modify
+ * it under the  terms of the GNU General  Public License as published
+ * by the Free  Software Foundation; either version 2  of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+ * MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.   See the GNU
+ * General Public License for more details.
+ */
+
 #include "fetch.h"
 #include "common.h"
 #include "version.h"
